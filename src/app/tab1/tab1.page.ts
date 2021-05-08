d08996be687bb258fe8a381a3d25ea8319ab08ee
luis_chacon01 <luis.chacon13@hotmail.com>
2021-05-08 16:58:00 -0500

    Semi_Pia

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Peliservice } from '../Services/pelis.service';
+import { Pelis } from '../model/Pelis.models'
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  results : Observable<Pelis>;
+  term : string='';
+  type : string='';
+
+
+  constructor(private peliService : Peliservice) {}
+
+  ngOnInit()
+  {
+
+  }
+
+  searchChanged(): void
+  {
+    console.log('searchChanged');
+    this.results = this.peliService.buscarPeliculas(this.term,this.type)
+  }
+}
